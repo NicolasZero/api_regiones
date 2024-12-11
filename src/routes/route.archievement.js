072156e195cb8e6d4a4e2d411c1667bed8baa361
Nicolas <nicojs2011@gmail.com>
2024-12-10 20:52:42 -0400

    actualizar logros

--- src/routes/route.archievement.js
+++ src/routes/route.archievement.js
@@ -4,4 +4,5 @@ module.exports = async function (fastify) {
   fastify.get("/", controller.getAllArchievement);
   fastify.get("/id/:id", controller.getArchievementById);
   fastify.post("/", controller.insertArchievement);
+  fastify.put("/", controller.updareArchievement);
 }
