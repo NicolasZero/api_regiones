e369aff9c85ad8a38eb264e0cf83da0592a91a70
Nicolas Zapata <nicojs2011@gmail.com>
2024-12-18 12:48:01 -0400

    fix crash en las rutas

--- src/routes/route.archievement.js
+++ src/routes/route.archievement.js
@@ -3,6 +3,6 @@ const controller = require("../controllers/controller.archievement");
 module.exports = async function (fastify) {
   fastify.get("/", controller.getAllArchievement);
   fastify.get("/id/:id", controller.getArchievementById);
-  fastify.get("/user/:id", controller.getArchievementByUser);
+  fastify.get("/user/:id", controller.getAllArchievementByUser);
   fastify.post("/", controller.insertArchievement);
 }
--- src/routes/route.schedule.js
+++ src/routes/route.schedule.js
@@ -3,6 +3,6 @@ const controller = require("../controllers/controller.schedule");
 module.exports = async function (fastify) {
   fastify.get("/", controller.getAllScheduled);
   fastify.get("/id/:id", controller.getScheduledById);
-  fastify.get("/user/:id", controller.getScheduledById);
+  fastify.get("/user/:id", controller.getAllScheduledbyUser);
   fastify.put("/", controller.updateScheduled);
 }
